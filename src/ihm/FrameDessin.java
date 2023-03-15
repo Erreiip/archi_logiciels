ce92523d1f466b90477313a855fc08442074f6fb
GloubyBoulga <81560542+GloubyBoulga@users.noreply.github.com>
2023-03-15 08:33:51 +0100

    ajout squelette des classes

---
+++ src/ihm/FrameDessin.java
@@ -0,0 +1,34 @@
+package src.ihm;
+
+import src.Controleur;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameDessin extends JFrame {
+    
+    private Controleur ctrl;
+    private PanelChoixActions panelHaut;
+    private JPanel panelDessin;
+    private PanelChoixCouleurs panelBas;
+
+    public FrameDessin(Controleur ctrl) {
+
+        this.ctrl = ctrl;
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        this.setLayout(new BorderLayout());
+        this.panelHaut = new PanelChoixActions(ctrl);
+        this.panelDessin = new JPanel();
+        this.panelBas = new PanelChoixCouleurs(ctrl);
+
+        this.add(this.panelHaut, BorderLayout.NORTH);
+        this.add(this.panelDessin, BorderLayout.CENTER);
+        this.add(this.panelBas, BorderLayout.SOUTH);
+        this.setVisible(true);
+    }
+}
---
+++ src/ihm/PanelChoixActions.java
@@ -0,0 +1,44 @@
+package src.ihm;
+
+import src.Controleur;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PanelChoixActions extends JPanel implements ActionListener {
+    
+    private Controleur ctrl;
+    private JButton[] boutonsActions;
+    private JCheckBox CBremplissage;
+    private String[] actions;
+
+    public PanelChoixActions(Controleur ctrl) {
+        this.ctrl = ctrl;
+        
+        this.actions = new String[]{"Cercle", "Rectangle", "Ligne", "Texte", "Effacer"};
+        this.boutonsActions = new JButton[this.actions.length];
+        this.setLayout(new GridLayout(1, this.actions.length, 10, 10));
+
+        for(int i = 0; i < this.actions.length; i++){
+            this.boutonsActions[i] = new JButton(this.actions[i]);
+            this.boutonsActions[i].setPreferredSize(new Dimension(30, 30));
+            this.boutonsActions[i].addActionListener(this);
+            this.add(this.boutonsActions[i]);
+        }
+        this.CBremplissage = new JCheckBox("Plein");
+        this.add(this.CBremplissage);
+    }
+
+
+    public boolean getCBremplissage() {
+        return this.CBremplissage.isSelected();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        for(int i = 0; i < this.actions.length; i++){
+            if(e.getSource() == this.boutonsActions[i]){
+                this.ctrl.setActionCourante(this.actions[i]);
+            }
+        }
+    }
+}
