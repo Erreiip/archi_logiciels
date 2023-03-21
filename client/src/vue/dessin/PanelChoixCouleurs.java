e577bd95cf35b033eeb67ecf0c1b6acc9851dd6c
Romain <romain.gascoin@gmail.com>
2023-03-21 09:53:04 +0100

    Updated color chosing method

--- client/src/vue/dessin/PanelChoixCouleurs.java
+++ client/src/vue/dessin/PanelChoixCouleurs.java
@@ -15,8 +15,8 @@ public class PanelChoixCouleurs extends JPanel implements ActionListener{
     public PanelChoixCouleurs(Controleur ctrl) {
         this.ctrl = ctrl;
         
-        this.couleurs = new Color[]{Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.PINK, Color.CYAN, Color.MAGENTA};
-        this.boutonsCouleurs = new JButton[this.couleurs.length];
+        this.couleurs = new Color[]{Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.CYAN, Color.MAGENTA};
+        this.boutonsCouleurs = new JButton[this.couleurs.length+1];
         this.setLayout(new GridLayout(1, this.couleurs.length, 10, 10));
 
         for(int i = 0; i < this.couleurs.length; i++){
@@ -26,13 +26,38 @@ public class PanelChoixCouleurs extends JPanel implements ActionListener{
             this.boutonsCouleurs[i].addActionListener(this);
             this.add(this.boutonsCouleurs[i]);
         }
+        this.boutonsCouleurs[this.couleurs.length] = new JButton("Couleurs");
+        this.boutonsCouleurs[this.couleurs.length].addActionListener(this);
+        this.add(this.boutonsCouleurs[this.couleurs.length]);
     }
 
     public void actionPerformed(ActionEvent e) {
-        for(int i = 0; i < this.couleurs.length; i++){
-            if(e.getSource() == this.boutonsCouleurs[i]){
-                this.ctrl.setCouleurCourante(this.couleurs[i]);
+        if(e.getSource() == this.boutonsCouleurs[this.couleurs.length]){
+
+            Color nouvelleCouleur = new JColorChooser().showDialog(this, "Choisir une couleur", this.ctrl.getCouleurCourante());
+            for (int i = 0; i < this.couleurs.length; i++) {
+                if (nouvelleCouleur.equals(this.couleurs[i]))
+                    this.ctrl.setCouleurCourante(this.couleurs[i]);
+            }
+
+            if (!nouvelleCouleur.equals(this.ctrl.getCouleurCourante())) {
+                this.ctrl.setCouleurCourante(nouvelleCouleur);
+
+                for(int j = this.couleurs.length - 1; j > 0; j--){
+                    this.couleurs[j] = this.couleurs[j - 1];
+                    this.boutonsCouleurs[j].setBackground(this.couleurs[j]);
+                }
+                this.couleurs[0] = this.ctrl.getCouleurCourante();
+                this.boutonsCouleurs[0].setBackground(this.couleurs[0]);
             }
         }
+        else {
+            for(int i = 0; i < this.couleurs.length; i++){
+                if(e.getSource() == this.boutonsCouleurs[i]){
+                    this.ctrl.setCouleurCourante(this.couleurs[i]);
+                }
+            }
+        }
+
     }
 }
